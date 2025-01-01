8446e8eddb64777584e7f9df1dad528041d758b4
unknown <amitjhah.2004@gmail.com>
2025-01-02 01:36:18 +0530

    6pattern

--- pattern/basic.cpp
+++ pattern/basic.cpp
@@ -15,9 +15,38 @@ void secondPattern(int row, int column){
         std::cout<<"\n";
     }
 }
+void numberPattern(int n){
+    for(int i = 0;i<=n;i++){
+        for(int j= 1; j<=i;j++){
+           std:: cout<<j; 
+        }
+        std::cout<<"\n";
+    }
+}
+
+void number2Pattern(int n){
+    int count = 1;
+    for(int i = 1;i<=n;i++){
+        for(int j= 1; j<=i;j++){
+           std:: cout<<count++; 
+        }
+        std::cout<<"\n";
+    }
+}
+
+
+void decreasepattern(int row,int column){
+    for(int i = 0;i<=row;i++){
+        for(int j = 1;j<=column-i;j++){
+            std::cout<<j;
+        }
+        std::cout<<"\n";
+    }
+}
+
 int main(){
-   
-    normalPattern(6);
+   decreasepattern(4,5);
+    number2Pattern(6);
     secondPattern(4,5);
     return 0;
 
