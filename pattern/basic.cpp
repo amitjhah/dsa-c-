b33d8d0cd9e448c5d593165226bf83b9965748db
unknown <amitjhah.2004@gmail.com>
2025-01-02 01:02:02 +0530

    secondpattern

--- pattern/basic.cpp
+++ pattern/basic.cpp
@@ -7,9 +7,18 @@ void normalPattern(int n){
         std::cout<<"\n";
     }
 }
+void secondPattern(int row, int column){
+    for(int i = 0;i<=row;i++){
+        for(int j = 0;j<=column;j++){
+            std::cout<<"*";
+        }
+        std::cout<<"\n";
+    }
+}
 int main(){
-    int n = 6;
-    normalPattern(n);
+   
+    normalPattern(6);
+    secondPattern(4,5);
     return 0;
 
 }
