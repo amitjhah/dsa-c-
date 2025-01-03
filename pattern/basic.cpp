eb255b581d6c46ee29c3bc4b8f27fefd7f6d4dd7
unknown <amitjhah.2004@gmail.com>
2025-01-04 00:04:23 +0530

    new pattern

--- pattern/basic.cpp
+++ pattern/basic.cpp
@@ -43,11 +43,46 @@ void decreasepattern(int row,int column){
         std::cout<<"\n";
     }
 }
+void tranglepattern(int n ){
+    for(int i =0;i<=n;i++){
+        for(int j =0;j<n-i-1;j++){
+            std::cout<<" ";
+        }
+        
+        for(int j =0;j<(2*i-1);j++){
+            std::cout<<"*";
+        }
+       
+        for(int j =0;j<n-i-1;j++){
+            std::cout<<" ";
+        }
+         std::cout<<"\n";
+    }
+}
+
+void tranglereverse(int n ){
+    for(int i =0;i<=n;i++){
+        for(int j =0;j<i;j++){
+            std::cout<<" ";
+        }
+        
+        for(int j =0;j<2*n-(2*i -1);j++){
+            std::cout<<"*";
+        }
+       
+        for(int j =0;j<i;j++){
+            std::cout<<" ";
+        }
+         std::cout<<"\n";
+    }
+}
 
 int main(){
    decreasepattern(4,5);
     number2Pattern(6);
     secondPattern(4,5);
+    tranglepattern(9);
+    tranglereverse(9);
     return 0;
 
 }
